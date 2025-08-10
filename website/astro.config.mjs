a7f2f0f8b38e00484ab6ab2b4c12be18e41164e0
Peyman Eskandari <peyman.eskandari@gmail.com>
2025-08-10 14:34:01 +0200

    add astro tailwind integrations

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { remarkAlert } from "remark-github-blockquote-alert";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://peyman.me",
-  integrations: [tailwind(), robotsTxt(), sitemap()],
+  integrations: [robotsTxt(), sitemap()],
   outDir: "../dist",
   markdown: {
     remarkPlugins: [remarkAlert],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
